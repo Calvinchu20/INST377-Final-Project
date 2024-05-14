da7a321152b49b33ef480addeb7322684910929f
Calvinchu20 <129889051+Calvinchu20@users.noreply.github.com>
2024-05-13 22:25:27 -0400

    editing

--- finalprojectjs.js
+++ finalprojectjs.js
@@ -1,3 +1,39 @@
-function holder() {
-    
+var host = window.location.origin;
+console.log(host)
+const ID = "d87eb441"
+const KEY = '6343bb2aa6042fa83f23e2c95a7931c7'
+function fetchmacronutrition(food){
+
+
 }
+async function createAnalysis(){
+    await fetch(`${host}/trackers`)
+    .then((res) => res.json())
+    .then((res) => {
+        console.log(res)
+
+    })
+
+}
+async function createUser(){
+    await fetch(`${host}/tracker`,{
+        method:"POST",
+        body: JSON.stringify({
+            "first_name": document.getElementById('firstName').value, 
+            "last_name": document.getElementById("lastName").value, 
+            "goal_calories":document.getElementById('calories').value,
+            "goal_fats":document.getElementById('fats').value,
+            "goal_carbs":document.getElementById('carbs').value,
+            "goal_protein":document.getElementById('protein').value,
+
+        }),
+        "Content-type": "application/json"
+    })
+    .then((res) => res.json())
+    .then((res) => {
+        console.log(res.body)
+    }) 
+    
+   
+}
+window.onload = createAnalysis();
