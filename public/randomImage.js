4baa238fc47ac141bd877cf73ae60ce8b1b47cb6
Calvinchu20 <129889051+Calvinchu20@users.noreply.github.com>
2024-05-14 16:20:15 -0400

    home

--- public/randomImage.js
+++ public/randomImage.js
@@ -1,12 +1,5 @@
 function displayImages(){
-    var counter = 0
 
-    fetch("https://foodish-api.com/images/rice")
-    .then((res)=>res.json())
-    .then((data)=>{
-        console.log(data)
-
-    })
 
 
 }
