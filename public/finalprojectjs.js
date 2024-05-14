b888283280f1e0343d82057d584d0fe3601af2fd
Calvinchu20 <129889051+Calvinchu20@users.noreply.github.com>
2024-05-14 01:16:41 -0400

    testing calculation

--- public/finalprojectjs.js
+++ public/finalprojectjs.js
@@ -3,26 +3,17 @@ console.log(host)
 const ID = "d87eb441"
 const KEY = '6343bb2aa6042fa83f23e2c95a7931c7'
 async function fetchmacronutrition(){
+    let food_list = document.getElementById("foodList")
+    var total_calories = 0;
+    var total_carbs = 0;
+    var total_protein = 0;
+    var total_fats = 0;
+    var fix_food = food_list.split(',').map(item=> item.trim())
+    console.log(fix_food)
+    for (let x = 0; x < fix_food.length; x++){
+        console.log(fix_food[x])
+    }
 
-    const headers = {
-        'Content-Type': 'application/json',
-        'x-app-id': 'd87eb441',
-        'x-app-key': '6343bb2aa6042fa83f23e2c95a7931c7',
-    };
-    const body = JSON.stringify({
-        query: "grape"
-    });
-    const temple = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-            method: 'POST',
-            headers: headers,
-            body: body
-    });
-
-    const nutrients = await temple.json();
-    //console.log(nutrients)
-    console.log(nutrients.foods[0].nf_protein);
-    const protein = nutrients.foods[0]
-    console.log('protein', protein.nf_protein)
 
 }
 async function createAnalysis(){
