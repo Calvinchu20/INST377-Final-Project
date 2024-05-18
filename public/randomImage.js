b8fc16bf8af5fed2104f944ffc30e8f55e913344
Deon Abellana <deon1123@terpmail.umd.edu>
2024-05-18 06:13:37 -0400

    Fixed UI + Added Nutrition Visualizer

--- public/randomImage.js
+++ public/randomImage.js
@@ -1,35 +1,28 @@
 function displayImages(){
- const imageBox = document.getElementById("foodImage")
- const health = document.createElement('img')
- const health2 = document.createElement('img')
- const health3 = document.createElement('img')
- const health4 = document.createElement('img')
- const health5 = document.createElement('img')
- health.src = 'https://i.etsystatic.com/15433923/r/il/97656a/1304482649/il_fullxfull.1304482649_evqg.jpg'
- health.height = '400'
- health.width = '300'
- imageBox.appendChild(health)
- health2.src = 'https://content.health.harvard.edu/wp-content/uploads/2023/09/d26a84e6-1146-48cf-92dd-d2fc8532b756.jpg'
- health2.height = '400'
- health2.width = '300'
- imageBox.appendChild(health2)
- health3.src = 'https://www.foodiesfeed.com/wp-content/uploads/2023/07/healthy-foods-selection.jpg'
- health3.height = '400'
- health3.width = '300'
- imageBox.appendChild(health3)
- health4.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Healthy_food.jpg/1024px-Healthy_food.jpg'
- health4.height = '400'
- health4.width = '300'
- imageBox.appendChild(health4)
- health5.src = 'https://cdn-prod.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods-laid-out-on-white-background.jpg'
- health5.height = '400'
- health.width = '300'
- imageBox.appendChild(health5)
- console.log(imageBox)
- simpleslider.getSlider({
+  const imageBox = document.getElementById("foodImage");
+  const images = [
+      'https://i.etsystatic.com/15433923/r/il/97656a/1304482649/il_fullxfull.1304482649_evqg.jpg',
+      'https://content.health.harvard.edu/wp-content/uploads/2023/09/d26a84e6-1146-48cf-92dd-d2fc8532b756.jpg',
+      'https://www.foodiesfeed.com/wp-content/uploads/2023/07/healthy-foods-selection.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Healthy_food.jpg/1024px-Healthy_food.jpg',
+      'https://cdn-prod.medicalnewstoday.com/content/images/articles/269/269143/popular-healthy-foods-laid-out-on-white-background.jpg'
+  ];
+
+  images.forEach(src => {
+      const img = document.createElement('img');
+      img.src = src;
+      img.alt = "Healthy food";
+      img.style.height = "400";
+      img.style.width = "300"; 
+      imageBox.appendChild(img);
+  });
+
+  simpleslider.getSlider({
     container: document.getElementById("foodImage")
     
   });
-
 }
+
 window.onload = displayImages();
+
+document.addEventListener('DOMContentLoaded', displayImages);
